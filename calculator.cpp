53f9520a16fef1dc1188e167c06acfa5fbb3aad7
VitaliiZZzz <fake.gutov@gmail.com>
2021-10-12 19:30:03 +0300

    My onw commit

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,22 @@
 #include "calculator.h"
 
+// Author: unkown
+// Version: 0.0.1
+// All rights reserved
+
+// add function
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
 }
 
+// sub function
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// mul fnction
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
